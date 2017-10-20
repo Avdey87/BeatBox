79e6c19d950ccd3317eaa034c6d325258dc2f6f2
avdey <avdey87@gmail.com>
2017-10-20 17:41:59 +0300

    Initial commit

---
+++ app/src/main/java/com/aavdeev/beatbox/BeatBoxFragment.java
@@ -0,0 +1,31 @@
+package com.aavdeev.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+//Преопределяем метод создания виью
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        //Создаем View переменную и записываем в нее вью элемент с параметрами
+        //получеными из fragment_beat_box.xml помещаем это в контейнер
+        //(создаем из лайоут файла вью элемент )
+        View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
+        RecyclerView recyclerView = (RecyclerView) view
+                .findViewById(R.id.fragment_beat_box_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return view;
+    }
+}
